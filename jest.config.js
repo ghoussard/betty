f15962e2b4eebdb7ca8bab024b070b9215afb194
Grégoire Houssard <gregoire.houssard@akeneo.com>
2022-02-14 19:12:55 +0100

    Remove useless jest path mapping

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,14 @@
 const nextJest = require('next/jest')
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
 })
 
-// Add any custom config to be passed to Jest
 const customJestConfig = {
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     '^@/back/(.*)$': '<rootDir>/src/back/$1',
     '^@/front/(.*)$': '<rootDir>/src/front/$1',
-    '^@/pages/(.*)$': '<rootDir>/src/pages/$1',
   },
   testPathIgnorePatterns: [
     '<rootDir>/node_modules/', 
@@ -25,5 +20,4 @@ const customJestConfig = {
   ]
 }
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig)
