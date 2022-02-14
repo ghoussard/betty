84a7eaf1b307a0a4385951efcd3cb46655a1c311
Grégoire Houssard <gregoire.houssard@akeneo.com>
2022-02-14 18:40:49 +0100

    Set up tailwindcss

--- src/front/components/HelloWorld.tsx
+++ src/front/components/HelloWorld.tsx
@@ -3,7 +3,7 @@ import { useHelloWorld } from '@/front/hooks'
 const HelloWorld = () => {
   const message = useHelloWorld()
 
-  return <div>{message}</div>
+  return <div className="text-3xl text-orange-500">{message}</div>
 }
 
 export { HelloWorld }
